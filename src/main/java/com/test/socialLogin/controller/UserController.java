088f15596d29a63cc0e8eb0298e580a5a541a150
koushik-ruidas <koushikruidas@gmail.com>
2022-07-12 18:38:33 +0530

    Final version -- jwt+oAuth with role

--- src/main/java/com/test/socialLogin/controller/UserController.java
+++ src/main/java/com/test/socialLogin/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.test.socialLogin.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.socialLogin.entity.User;
@@ -10,12 +18,16 @@ import com.test.socialLogin.exception.ResourceNotFoundException;
 import com.test.socialLogin.repository.UserRepository;
 import com.test.socialLogin.security.CurrentUser;
 import com.test.socialLogin.security.UserPrincipal;
+import com.test.socialLogin.service.IHomeService;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+	private IHomeService homeService;
 
     @GetMapping("/user/me")
     @PreAuthorize("hasRole('USER')")
@@ -23,4 +35,20 @@ public class UserController {
         return userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
     }
+    
+	@GetMapping("/getUsers")
+	public ResponseEntity<List<User>> getUsers(){
+		return new ResponseEntity<List<User>>(homeService.getUsers(), HttpStatus.OK);
+	}
+
+	@Secured({"ROLE_ADMIN"})
+	@GetMapping("/getById/{id}")
+	public Optional<User> getUser(@PathVariable Long id) {
+		return homeService.findById(id);
+	}
+	
+	@GetMapping("/index")
+	public String index() {
+		return "Welcome to the index page";
+	}
 }
